bcf332836c66f307a884935697960c9ef10b25bc
Sam Rao <samrao1997@gmail.com>
2023-08-09 14:06:44 -0700

    Get rid of answers

--- graph_template.py
+++ graph_template.py
@@ -1,17 +1,8 @@
-
 """
-Empty Graph Template to implement :D
-
-YouTube Kylie Ying: https://www.youtube.com/ycubed 
-Twitch KylieYing: https://www.twitch.tv/kylieying 
-Twitter @kylieyying: https://twitter.com/kylieyying 
-Instagram @kylieyying: https://www.instagram.com/kylieyying/ 
-Website: https://www.kylieying.com
-Github: https://www.github.com/kying18 
-Programmer Beast Mode Spotify playlist: https://open.spotify.com/playlist/4Akns5EUb3gzmlXIdsJkPs?si=qGc4ubKRRYmPHAJAIrCxVQ 
 """
 
 import random
+
 
 class Vertex(object):
     def __init__(self, value):
@@ -32,7 +23,6 @@ class Vertex(object):
 
     def next_word(self):
         pass
-
 
 
 class Graph(object):
